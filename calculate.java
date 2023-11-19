69e738e56da6f75dd12237ee3a8ddcfe3c7c9c8b
praveen14012004 <mr.praveensinghchauhan14@gmail.com>
2023-11-19 17:34:36 +0530

    New variables for calculator

---
+++ calculate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+ * calculate
+ */
+public class calculate {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
